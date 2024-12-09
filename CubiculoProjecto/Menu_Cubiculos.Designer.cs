f61138ea216ecc5305327a4377779e28bb69336f
Rodrigo <rodriusamac@hotmail.com>
2024-12-09 14:39:50 -0700

    Prueba

--- CubiculoProjecto/Menu_Cubiculos.Designer.cs
+++ CubiculoProjecto/Menu_Cubiculos.Designer.cs
@@ -179,7 +179,7 @@
             this.label1.BackColor = System.Drawing.Color.DarkGoldenrod;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(173, 483);
+            this.label1.Location = new System.Drawing.Point(166, 483);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(445, 30);
             this.label1.TabIndex = 11;
@@ -193,7 +193,7 @@
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(816, 610);
+            this.ClientSize = new System.Drawing.Size(800, 571);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblFecha);
             this.Controls.Add(this.lblReloj);
